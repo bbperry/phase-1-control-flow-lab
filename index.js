da52d5cd2ec4b06bba66f18ee8c162688af73018
Ben <benbperry1@gmail.com>
2021-11-01 16:13:30 -0700

    commit tree 9eb6450645ad498e5c095a2c318b4417cd38a8fa

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+  return 'This one is on me!'
+  } else if (value > 2000 && value < 2500) {
+    return 'I will gladly take your thirty bucks.' 
+} else if (value > 2500) {
+  return 'No can do.'
+}
+} 
+
+
+function ternaryCheckCity(city){
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous" : return 'Thank you so much.';
+    case "not as generous" : return 'Thank you.'
+    case 'thanks for everything' : return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
